80d93b87ebada13c4e657cd8d8eb1d844c4fa130
Muhammad Umar <umarabdulhaque@gmail.com>
2025-05-15 17:41:13 +0500

    fix:upload error

--- src/middleware/uploadMiddleware.js
+++ src/middleware/uploadMiddleware.js
@@ -1,7 +1,7 @@
 import multer from "multer";
 
 const storage = multer.diskStorage({
-  destination: "uplaods/",
+  destination: "uploads/",
   filename: function (req, file, cb) {
     cb(null, Date.now() + "-" + file.originalname);
   },
