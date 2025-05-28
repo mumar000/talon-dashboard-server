da337def43e81693b89eef4c80e2725b77d82e3f
Muhammad Umar <umarabdulhaque@gmail.com>
2025-05-28 10:57:05 +0500

    Fix:jwt expire time increase

--- utils/generateToken.js
+++ utils/generateToken.js
@@ -9,7 +9,7 @@ const generateToken = (res, userId) => {
     httpOnly: true,
     secure: process.env.NODE_ENV !== "development",
     sameSite: process.env.NODE_ENV !== "development" ? "None" : "Lax",
-    maxAge: 30 * 24 * 60 * 1000,
+    maxAge: 30 * 24 * 60 * 60 * 1000,
   });
 };
 
